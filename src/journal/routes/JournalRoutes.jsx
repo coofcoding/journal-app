5147ece355577b61fdd7061a55deac41f5b98317
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-01 17:42:18 -0500

    configuring the files managment and importing reactrouter

---
+++ src/journal/routes/JournalRoutes.jsx
@@ -0,0 +1,11 @@
+import { JournalPage } from "../pages/JournalPage";
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const JournalRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<JournalPage />} />
+            <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+    )
+}
