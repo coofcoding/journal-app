2f529bf27456fa7124e27a873a8f69908b15fb59
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-12 15:47:42 -0500

    installing dotenv, configuring environment variables

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,12 @@
 // jest.setup.js
 // In case of need the useFetch() importation
 import 'whatwg-fetch'; // <-- pnpm add whatwg-fetch
+import { getEnvironments } from './src/helpers/getEnvironments';
+
+require('dotenv').config({
+    path: '.env.test'
+})
+
+jest.mock('./src/helpers/getEnvironments', () => {
+    getEnvironments: () => ({ ...process.env })
+})
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,18 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
+import { getEnvironments } from "../helpers/getEnvironments";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const env = getEnvironments();
+
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId
-// };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
+};
 
 // official
 // const firebaseConfig = {
@@ -25,16 +28,16 @@ import { getFirestore } from 'firebase/firestore/lite';
 //   appId: '1:222508904403:web:1cc06249c6da112d25c926'
 // };
 
-// for testing
-const firebaseConfig = {
-  apiKey: 'AIzaSyBw5BybaG17TW9lOwkqJJ2A9YHFr_PQPEY',
-  authDomain: 'testing-d6945.firebaseapp.com',
-  projectId: 'testing-d6945',
-  storageBucket: 'testing-d6945.appspot.com',
-  messagingSenderId: '463457247371',
-  appId: '1:463457247371:web:c96b8873931e085418348e',
-  measurementId: "G-8MEDNC9443"
-};
+// // for testing
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBw5BybaG17TW9lOwkqJJ2A9YHFr_PQPEY',
+//   authDomain: 'testing-d6945.firebaseapp.com',
+//   projectId: 'testing-d6945',
+//   storageBucket: 'testing-d6945.appspot.com',
+//   messagingSenderId: '463457247371',
+//   appId: '1:463457247371:web:c96b8873931e085418348e',
+//   measurementId: "G-8MEDNC9443"
+// };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
